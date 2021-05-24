34f5373114e713f02c37e94e84fbff831e2b268c
Nathan Malkin <nmalkin@cs.berkeley.edu>
2021-05-24 13:12:51 -0700

    Colorize exceptions when they occur

--- qualtrics2sheets/src/main/kotlin/q2s/Main.kt
+++ qualtrics2sheets/src/main/kotlin/q2s/Main.kt
@@ -1,10 +1,13 @@
 package q2s
 
 import kotlinx.cli.ArgParser
+import logging.KotlinLogging
 import q2s.qualtrics.DownloadQualtricsCommand
 import q2s.sheets.UploadCSVCommand
 
 const val PROGRAM_NAME = "qualtrics2csv"
+
+private val logger = KotlinLogging.logger { }
 
 var subcommandFinished = false
 
@@ -12,7 +15,14 @@ var subcommandFinished = false
 fun main(args: Array<String>) {
     val parser = ArgParser(PROGRAM_NAME)
     parser.subcommands(ExportAndUploadCommand(), UploadCSVCommand(), DownloadQualtricsCommand())
-    parser.parse(args)
+
+    try {
+        parser.parse(args)
+    } catch (err: Exception) {
+        logger.error("execution failed: $err")
+        err.printStackTrace()
+        return
+    }
 
     if (!subcommandFinished) {
         System.err.println("--help or -h for usage")
